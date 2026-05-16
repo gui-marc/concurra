2fc072ced140c0347c4150ab2af94260d9d32808
Guilherme Marcondes <guisupre@gmail.com>
2026-05-16 18:44:03 +0100

    feat(scheduler-ui): bootstrap app structure

--- scheduler-ui/src/main.tsx
+++ scheduler-ui/src/main.tsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { RouterProvider } from "react-router"
+import { router } from "./routes/routes"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 )
---
+++ scheduler-ui/src/routes/routes.ts
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router"
+
+import RootLayout from "@/layout/root-layout"
+import EventPage from "@/pages/event-page"
+import EventsPage from "@/pages/events-page"
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        path: "events",
+        Component: EventsPage,
+      },
+      {
+        path: "events/:event",
+        Component: EventPage,
+      },
+    ],
+  },
+])
